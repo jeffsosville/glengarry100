348ed752c4e6aa88cfeada74f313f7f327b70f33
jeffsosville <148246966+jeffsosville@users.noreply.github.com>
2025-05-13 14:25:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def load_data():
     brokers_df = pd.DataFrame(brokers_resp.data)
     listings_df = pd.DataFrame(listings_resp.data)
     return brokers_df, listings_df
+    
 st.write("Sample listings broker_ids:", listings_df["broker_id"].dropna().astype(str).unique()[:10])
 
 
