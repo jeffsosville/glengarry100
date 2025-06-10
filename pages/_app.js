5a108f97b4c8076921c23612912c823a8332c202
jasosville <31511476+jasosville@users.noreply.github.com>
2025-06-10 15:04:50 -0400

    feat: Add global navigation menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css';
+import '../styles/globals.css'; // This imports your global styles, usually Tailwind CSS
 
+// Import the Layout component you created
+import Layout from '../components/Layout';
+
+// This is the main Application component for Next.js
+// It wraps all your individual pages
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    // Wrap the current page (Component) with your custom Layout
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
